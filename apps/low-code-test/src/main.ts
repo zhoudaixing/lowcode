98e68d34eadc639aaec6bbc89a77f4b8732b24f1
zdx <dreamzz2000@163.com>
2024-06-19 11:27:26 +0800

    feat: 增加接口文档Swagger

--- apps/low-code-test/src/main.ts
+++ apps/low-code-test/src/main.ts
@@ -9,17 +9,21 @@ import {
   HttpExceptionFilter,
 } from '@app/comm';
 
+import { generateDocument } from './doc';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
   // Version Control
   app.enableVersioning({
     type: VersioningType.URI,
   });
 
-  app.useGlobalInterceptors(new TransformInterceptor());
-
-  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  generateDocument(app);
 
   await app.listen(3000);
 }
